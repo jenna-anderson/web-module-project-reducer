f1ab0ce2ae6c4b6dc6888c3b4ae9c55737ec6c9f
Jenna Anderson <jennaanderson@Jennas-MacBook-Air.local>
2021-06-07 14:38:46 -0600

    connected premade add one action and answered understanding question

--- Understanding-Questions.md
+++ Understanding-Questions.md
@@ -1,7 +1,30 @@
 # Understanding Questions:
 1. What are the steps of execution from the pressing of the 1 button to the rendering of our updated value? List what part of the code excutes for each step.
 * The user presses the 1 button.
-* 
-...
+* onClick fires the handleOneClick function 
+    <CalcButton onClick={handleOneClick} value={1}/>
+
+* handleOneClick function fires addOne() action creator
+    const handleOneClick = () => {
+    dispatch(addOne());
+}
+
+* addOne action creator in the actions folder returns the action type ADD_ONE (which is "ADD_ONE")
+    export const addOne = () => {
+    return({type:ADD_ONE});
+}
+
+*  The dispatch function has access to the reducer function which will take the action returned from the previous step and the current state as arguments. Since ADD_ONE is the action then that case will execute and 1 will be added to the current state.total and returned.
+    const reducer = (state, action) => {
+    switch(action.type) {
+        case(ADD_ONE):
+            return({
+                ...state,
+                total: state.total + 1
+            });
+
+* Now 1 will display because state.total is 1.
+    <TotalDisplay value={state.total}/>
+
 
 * TotalDisplay shows the total plus 1.
--- src/App.js
+++ src/App.js
@@ -5,7 +5,17 @@ import './App.css';
 import TotalDisplay from './components/TotalDisplay';
 import CalcButton from './components/CalcButton';
 
+import reducer, { initialState } from './reducers';
+import { useReducer } from 'react';
+
+import { addOne } from './actions';
+
 function App() {
+const [state, dispatch] = useReducer(reducer, initialState);
+
+const handleOneClick = () => {
+  dispatch(addOne());
+}
 
   return (
     <div className="App">
@@ -17,10 +27,10 @@ function App() {
         <div className="col-md-12 d-flex justify-content-center">
           <form name="Cal">
             
-            <TotalDisplay value={0}/>
+            <TotalDisplay value={state.total}/>
             <div className="row details">
-              <span id="operation"><b>Operation:</b> X</span>
-              <span id="memory"><b>Memory:</b> 0</span>
+              <span id="operation"><b>Operation:</b> {state.operation} </span>
+              <span id="memory"><b>Memory:</b> {state.memory} </span>
             </div>
             
             <div className="row">
@@ -30,7 +40,7 @@ function App() {
             </div>
 
             <div className="row">
-              <CalcButton value={1}/>
+              <CalcButton onClick={handleOneClick} value={1}/>
               <CalcButton value={2}/>
               <CalcButton value={3}/>
             </div>
